9b4b31b7622f5a884e69fc9c0a665e7652a3b4ef
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2023-10-03 14:48:27 -0300

    Feat: Create Authentication use case

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,34 @@
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const authenticateUserCase = makeAuthenticateUseCase()
+
+    await authenticateUserCase.execute({
+      email,
+      password,
+    })
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(409).send({
+        message: err.message,
+      })
+    }
+    return err
+  }
+
+  return reply.status(200).send()
+}
