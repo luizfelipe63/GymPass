c8be795ab7f6319b3069c87c0565b0e6c0c7197d
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2023-11-12 15:18:03 -0300

    Feat: Refreshen token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,11 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './register'
-import { authenticate } from './authenticate'
 import { profile } from './profile'
+import { refresh } from './refresh'
+import { authenticate } from './authenticate'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
   app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
